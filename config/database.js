e01fa6cd6859cb3b17f6e2137750c4c1a2ea2d0f
nanirama <nani.rama1982@gmail.com>
2022-06-17 06:48:57 +0530

    n change3

--- config/database.js
+++ config/database.js
@@ -2,21 +2,14 @@ const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-      },
-      options: {
-        ssl: false,
-      },
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
-  },
+  }
 });
