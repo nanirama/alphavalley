1b503987f8534ffbad484110517734e47458812b
nanirama <nani.rama1982@gmail.com>
2022-06-17 06:43:05 +0530

    n change2

--- config/database.js
+++ config/database.js
@@ -1,13 +1,22 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'alphav_db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'root'),
-      ssl: env.bool('DATABASE_SSL', false),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {
+        ssl: false,
+      },
     },
   },
 });
